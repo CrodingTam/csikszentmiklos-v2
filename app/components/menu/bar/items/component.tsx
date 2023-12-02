c7b89b57989a4e941c25665969606bb15256ab8f
SzTamass <sz.tamas@camelcoding.com>
2023-12-02 14:18:49 +0200

    10: navigation and page updates -updated header

--- app/components/menu/bar/items/component.tsx
+++ app/components/menu/bar/items/component.tsx
@@ -1,24 +1,56 @@
 "use client";
 
 import { PageModel } from "@/app/models/page.model";
-import { getLeftPageItems, getRightPageItems } from "@/app/utils/page.util";
+import { getAllPageItem, getLeftPageItems, getRightPageItems } from "@/app/utils/page.util";
 import { Box, ListItem, Typography } from "@mui/material";
-import { FC } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import StyledWrapper from "./style";
 import Img from "@/app/components/image/component";
 import useNavigation from "@/app/hooks/navigation.hook";
 import CustomLink from "@/app/components/link/component";
+import { usePathname } from "next/navigation";
+import { pages } from "@/app/constants/page.constant";
 
 const MenuItems: FC = () => {
   const { to } = useNavigation();
   const rightItems: PageModel[] = getRightPageItems();
   const leftItems: PageModel[] = getLeftPageItems();
+  const [activeItem, setActiveItem] = useState<PageModel | undefined>(undefined);
+  const pathName = usePathname();
+
+  const handleActiveItemChange = useCallback(() => {
+    let page = pages.find((item) => item.route === pathName);
+    setActiveItem(page);
+  }, [pathName]);
+
+  //  const handleMouseEnter = (page: PageModel) => {
+  //    setActiveItem(page);
+  //  };
+
+  const handleMouseLeave = () => {
+    handleActiveItemChange();
+  };
+
+  useEffect(() => {
+    handleActiveItemChange();
+  }, [handleActiveItemChange, pathName]);
 
   const renderPageItem = (item: PageModel, index: number): JSX.Element => {
     return (
-      <ListItem key={index} className="page-item">
-        <CustomLink url={item.route}>
-          <Typography variant="subtitle1">{item.label}</Typography>
+      <ListItem key={index} className="page-item" onClick={() => handleActiveItemChange()}>
+        <CustomLink
+          className={activeItem?.route === item.route ? "active-link" : "inactive-link"}
+          url={item.route}
+        >
+          <Typography
+            className="label"
+            variant="subtitle1"
+            sx={{
+              color: activeItem?.route === item.route ? "primary.main" : "black !important",
+            }}
+          >
+            {item.label}
+          </Typography>
         </CustomLink>
       </ListItem>
     );
